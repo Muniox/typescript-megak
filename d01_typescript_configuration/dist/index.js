3c7a594624edd8abdd028aed176ff3bdb7eb5557
Muniox <muniox20@gmail.com>
2022-10-03 22:33:58 +0200

    d01 typescript creating tsconfig.json

---
+++ d01_typescript_configuration/dist/index.js
@@ -0,0 +1,9 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const node_path_1 = __importDefault(require("node:path"));
+console.log(node_path_1.default.join(__dirname, __filename));
+console.log("https://www.typescriptlang.org/tsconfig");
+//# sourceMappingURL=index.js.map
---
+++ d01_typescript_configuration/index.ts
@@ -0,0 +1,6 @@
+import path from "node:path";
+
+
+console.log(path.join(__dirname, __filename));
+
+console.log("https://www.typescriptlang.org/tsconfig");
